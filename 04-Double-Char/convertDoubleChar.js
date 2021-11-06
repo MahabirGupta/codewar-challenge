df91e08c48a048341d244345fd08eaaec62a9b99
Mahabir <mahabirg81@gmail.com>
2021-11-06 19:55:32 +0800

    Double-Char changes made

--- 04-Double-Char/convertDoubleChar.js
+++ 04-Double-Char/convertDoubleChar.js
@@ -1,7 +1,18 @@
 const DoubleChar = (string) => {
   
   console.log("Convert string to repeated characters")
-  return string.split(" ");
-};
+  const s = "abcd";
+  
+  console.log (s.split('').map(function(v) {
+    // iterate and update
+    console.log(v+v);
+
+    return v + v;
+    // join the updated array
+  }).join('')
+  //console.log(s);
+ 
+  
+  )};
 
 module.exports = { DoubleChar }
